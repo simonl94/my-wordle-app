0e298265022132f117064c78cc022094a278d24c
Simon <slee1994@hotmail.com>
2022-07-07 00:47:50 +1000

    Added global styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 
 const Home: NextPage = () => {
-  return <div className="text-2xl text-red-200"> Hello world </div>;
+  return <div> Hello world </div>;
 };
 
 export default Home;
